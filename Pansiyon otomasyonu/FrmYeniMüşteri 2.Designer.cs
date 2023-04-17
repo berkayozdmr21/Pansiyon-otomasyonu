3187ff64931c51726ee3afc5ce0c500e58d1cfab
berkayozdmr21 <berkay.oz3321@gmail.com>
2023-04-17 15:14:28 +0300

    Proje dosyası ekle.

---
+++ Pansiyon otomasyonu/FrmYeniMüşteri 2.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace Pansiyon_otomasyonu
+{
+    partial class FrmYeniMüşteri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.DtpÇıkışTarihi = new System.Windows.Forms.DateTimePicker();
+            this.DtpGirisTarihi = new System.Windows.Forms.DateTimePicker();
+            this.MskTxtTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.TxtOdaNo = new System.Windows.Forms.TextBox();
+            this.TxtKimlikNo = new System.Windows.Forms.TextBox();
+            this.TxtSoyadi = new System.Windows.Forms.TextBox();
+            this.TxtAdi = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnA9 = new System.Windows.Forms.Button();
+            this.BtnA8 = new System.Windows.Forms.Button();
+            this.BtnA7 = new System.Windows.Forms.Button();
+            this.BtnA6 = new System.Windows.Forms.Button();
+            this.BtnA5 = new System.Windows.Forms.Button();
+            this.BtnA4 = new System.Windows.Forms.Button();
+            this.BtnA3 = new System.Windows.Forms.Button();
+            this.BtnA2 = new System.Windows.Forms.Button();
+            this.BtnA1 = new System.Windows.Forms.Button();
+            this.BtnBoşOda = new System.Windows.Forms.Button();
+            this.BtnDoluOda = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.BtnKaydet);
+            this.groupBox1.Controls.Add(this.DtpÇıkışTarihi);
+            this.groupBox1.Controls.Add(this.DtpGirisTarihi);
+            this.groupBox1.Controls.Add(this.MskTxtTelefon);
+            this.groupBox1.Controls.Add(this.TxtOdaNo);
+            this.groupBox1.Controls.Add(this.TxtKimlikNo);
+            this.groupBox1.Controls.Add(this.TxtSoyadi);
+            this.groupBox1.Controls.Add(this.TxtAdi);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 350);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri Bilgileri";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(126, 299);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(89, 28);
+            this.BtnKaydet.TabIndex = 30;
+            this.BtnKaydet.Text = "Kaydet\r\n\r\n";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // DtpÇıkışTarihi
+            // 
+            this.DtpÇıkışTarihi.Location = new System.Drawing.Point(95, 259);
+            this.DtpÇıkışTarihi.Name = "DtpÇıkışTarihi";
+            this.DtpÇıkışTarihi.Size = new System.Drawing.Size(169, 23);
+            this.DtpÇıkışTarihi.TabIndex = 29;
+            // 
+            // DtpGirisTarihi
+            // 
+            this.DtpGirisTarihi.Location = new System.Drawing.Point(95, 230);
+            this.DtpGirisTarihi.Name = "DtpGirisTarihi";
+            this.DtpGirisTarihi.Size = new System.Drawing.Size(169, 23);
+            this.DtpGirisTarihi.TabIndex = 28;
+            // 
+            // MskTxtTelefon
+            // 
+            this.MskTxtTelefon.BackColor = System.Drawing.Color.LightGray;
+            this.MskTxtTelefon.Location = new System.Drawing.Point(97, 102);
+            this.MskTxtTelefon.Mask = "(999) 000-0000";
+            this.MskTxtTelefon.Name = "MskTxtTelefon";
+            this.MskTxtTelefon.Size = new System.Drawing.Size(167, 23);
+            this.MskTxtTelefon.TabIndex = 27;
+            // 
+            // TxtOdaNo
+            // 
+            this.TxtOdaNo.BackColor = System.Drawing.Color.LightGray;
+            this.TxtOdaNo.Location = new System.Drawing.Point(95, 173);
+            this.TxtOdaNo.Name = "TxtOdaNo";
+            this.TxtOdaNo.Size = new System.Drawing.Size(169, 23);
+            this.TxtOdaNo.TabIndex = 26;
+            // 
+            // TxtKimlikNo
+            // 
+            this.TxtKimlikNo.BackColor = System.Drawing.Color.LightGray;
+            this.TxtKimlikNo.Location = new System.Drawing.Point(95, 138);
+            this.TxtKimlikNo.Name = "TxtKimlikNo";
+            this.TxtKimlikNo.Size = new System.Drawing.Size(169, 23);
+            this.TxtKimlikNo.TabIndex = 25;
+            // 
+            // TxtSoyadi
+            // 
+            this.TxtSoyadi.BackColor = System.Drawing.Color.LightGray;
+            this.TxtSoyadi.Location = new System.Drawing.Point(95, 73);
+            this.TxtSoyadi.Name = "TxtSoyadi";
+            this.TxtSoyadi.Size = new System.Drawing.Size(167, 23);
+            this.TxtSoyadi.TabIndex = 24;
+            // 
+            // TxtAdi
+            // 
+            this.TxtAdi.BackColor = System.Drawing.Color.LightGray;
+            this.TxtAdi.Location = new System.Drawing.Point(95, 44);
+            this.TxtAdi.Name = "TxtAdi";
+            this.TxtAdi.Size = new System.Drawing.Size(167, 23);
+            this.TxtAdi.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 15);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Çıkış  Tarihi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 15);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Giriş Tarihi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 15);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Oda Numarası:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "T.C. Kimlik NO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Telefon:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Adı:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BtnA9);
+            this.groupBox2.Controls.Add(this.BtnA8);
+            this.groupBox2.Controls.Add(this.BtnA7);
+            this.groupBox2.Controls.Add(this.BtnA6);
+            this.groupBox2.Controls.Add(this.BtnA5);
+            this.groupBox2.Controls.Add(this.BtnA4);
+            this.groupBox2.Controls.Add(this.BtnA3);
+            this.groupBox2.Controls.Add(this.BtnA2);
+            this.groupBox2.Controls.Add(this.BtnA1);
+            this.groupBox2.Location = new System.Drawing.Point(441, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(430, 350);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // BtnA9
+            // 
+            this.BtnA9.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA9.Location = new System.Drawing.Point(294, 134);
+            this.BtnA9.Name = "BtnA9";
+            this.BtnA9.Size = new System.Drawing.Size(75, 23);
+            this.BtnA9.TabIndex = 8;
+            this.BtnA9.Text = "A-9";
+            this.BtnA9.UseVisualStyleBackColor = false;
+            // 
+            // BtnA8
+            // 
+            this.BtnA8.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA8.Location = new System.Drawing.Point(158, 134);
+            this.BtnA8.Name = "BtnA8";
+            this.BtnA8.Size = new System.Drawing.Size(75, 23);
+            this.BtnA8.TabIndex = 7;
+            this.BtnA8.Text = "A-8";
+            this.BtnA8.UseVisualStyleBackColor = false;
+            // 
+            // BtnA7
+            // 
+            this.BtnA7.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA7.Location = new System.Drawing.Point(43, 130);
+            this.BtnA7.Name = "BtnA7";
+            this.BtnA7.Size = new System.Drawing.Size(75, 23);
+            this.BtnA7.TabIndex = 6;
+            this.BtnA7.Text = "A-7";
+            this.BtnA7.UseVisualStyleBackColor = false;
+            // 
+            // BtnA6
+            // 
+            this.BtnA6.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA6.Location = new System.Drawing.Point(294, 83);
+            this.BtnA6.Name = "BtnA6";
+            this.BtnA6.Size = new System.Drawing.Size(75, 24);
+            this.BtnA6.TabIndex = 5;
+            this.BtnA6.Text = "A-6";
+            this.BtnA6.UseVisualStyleBackColor = false;
+            // 
+            // BtnA5
+            // 
+            this.BtnA5.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA5.Location = new System.Drawing.Point(158, 83);
+            this.BtnA5.Name = "BtnA5";
+            this.BtnA5.Size = new System.Drawing.Size(75, 23);
+            this.BtnA5.TabIndex = 4;
+            this.BtnA5.Text = "A-5";
+            this.BtnA5.UseVisualStyleBackColor = false;
+            // 
+            // BtnA4
+            // 
+            this.BtnA4.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA4.Location = new System.Drawing.Point(43, 83);
+            this.BtnA4.Name = "BtnA4";
+            this.BtnA4.Size = new System.Drawing.Size(75, 23);
+            this.BtnA4.TabIndex = 3;
+            this.BtnA4.Text = "A-4";
+            this.BtnA4.UseVisualStyleBackColor = false;
+            // 
+            // BtnA3
+            // 
+            this.BtnA3.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA3.Location = new System.Drawing.Point(294, 36);
+            this.BtnA3.Name = "BtnA3";
+            this.BtnA3.Size = new System.Drawing.Size(75, 23);
+            this.BtnA3.TabIndex = 2;
+            this.BtnA3.Text = "A-3";
+            this.BtnA3.UseVisualStyleBackColor = false;
+            // 
+            // BtnA2
+            // 
+            this.BtnA2.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA2.Location = new System.Drawing.Point(158, 33);
+            this.BtnA2.Name = "BtnA2";
+            this.BtnA2.Size = new System.Drawing.Size(75, 23);
+            this.BtnA2.TabIndex = 1;
+            this.BtnA2.Text = "A-2";
+            this.BtnA2.UseVisualStyleBackColor = false;
+            // 
+            // BtnA1
+            // 
+            this.BtnA1.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnA1.Location = new System.Drawing.Point(43, 33);
+            this.BtnA1.Name = "BtnA1";
+            this.BtnA1.Size = new System.Drawing.Size(75, 23);
+            this.BtnA1.TabIndex = 0;
+            this.BtnA1.Text = "A-1";
+            this.BtnA1.UseVisualStyleBackColor = false;
+            // 
+            // BtnBoşOda
+            // 
+            this.BtnBoşOda.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnBoşOda.Location = new System.Drawing.Point(735, 386);
+            this.BtnBoşOda.Name = "BtnBoşOda";
+            this.BtnBoşOda.Size = new System.Drawing.Size(75, 23);
+            this.BtnBoşOda.TabIndex = 9;
+            this.BtnBoşOda.Text = "Boş";
+            this.BtnBoşOda.UseVisualStyleBackColor = false;
+            // 
+            // BtnDoluOda
+            // 
+            this.BtnDoluOda.BackColor = System.Drawing.Color.Red;
+            this.BtnDoluOda.Location = new System.Drawing.Point(735, 415);
+            this.BtnDoluOda.Name = "BtnDoluOda";
+            this.BtnDoluOda.Size = new System.Drawing.Size(75, 23);
+            this.BtnDoluOda.TabIndex = 10;
+            this.BtnDoluOda.Text = "Dolu";
+            this.BtnDoluOda.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.Location = new System.Drawing.Point(97, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 23);
+            this.textBox1.TabIndex = 32;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 205);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 15);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Ücret:";
+            // 
+            // FrmYeniMüşteri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(916, 450);
+            this.Controls.Add(this.BtnDoluOda);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.BtnBoşOda);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmYeniMüşteri";
+            this.Text = "Yeni Müşteri Ekle";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button BtnKaydet;
+        private DateTimePicker DtpÇıkışTarihi;
+        private DateTimePicker DtpGirisTarihi;
+        private MaskedTextBox MskTxtTelefon;
+        private TextBox TxtOdaNo;
+        private TextBox TxtKimlikNo;
+        private TextBox TxtSoyadi;
+        private TextBox TxtAdi;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Button BtnA9;
+        private Button BtnA8;
+        private Button BtnA7;
+        private Button BtnA6;
+        private Button BtnA5;
+        private Button BtnA4;
+        private Button BtnA3;
+        private Button BtnA2;
+        private Button BtnA1;
+        private Button BtnBoşOda;
+        private Button BtnDoluOda;
+        private TextBox textBox1;
+        private Label label8;
+    }
+}
